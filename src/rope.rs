e0896373fca10dd4c9dc24a1e9acac2cd1862122
Gary Holland <garyhollandxyz@gmail.com>
2021-05-12 08:32:35 +0100

    wip: zip to point

--- src/rope.rs
+++ src/rope.rs
@@ -1,11 +1,11 @@
 use std::cmp::Ordering;
 
 use bevy::prelude::*;
-use bevy_rapier2d::na::distance;
 use bevy_rapier2d::na::{center, Point2};
 use bevy_rapier2d::physics::JointBuilderComponent;
 use bevy_rapier2d::rapier::dynamics::{BallJoint, RigidBodyBuilder};
 use bevy_rapier2d::rapier::geometry::ColliderBuilder;
+use bevy_rapier2d::{na::distance, physics::RigidBodyHandleComponent, rapier::dynamics::RigidBody};
 
 use crate::{anchor_point::AnchorPoint, ball::Ball, Materials};
 
@@ -13,7 +13,7 @@ pub struct RopePlugin;
 
 impl Plugin for RopePlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_system_to_stage(CoreStage::PreUpdate, toggle_rope.system());
+        app.add_system(toggle_rope.system());
     }
 }
 pub struct Rope;
@@ -39,11 +39,16 @@ pub fn spawn_rope(
     let rope_collider = ColliderBuilder::cuboid(rope_width / 2., rope_length / 2.);
     let rope_size = Vec2::new(rope_width, rope_length);
 
+    let mut rope_transformation =
+        Transform::from_translation(Vec3::new(middle_point.x, middle_point.y, 0.));
+    rope_transformation.rotate(Quat::from_rotation_y(angle));
+
     let rope = commands
         .spawn()
         .insert(Rope)
         .insert_bundle((rope_body, rope_collider))
         .insert_bundle(SpriteBundle {
+            transform: rope_transformation,
             material,
             sprite: Sprite::new(rope_size),
             ..Default::default()
@@ -81,7 +86,6 @@ pub fn toggle_rope(
         if let Ok(ball) = ball_query.single() {
             // get closest anchor
             let ball_point = Point2::new(ball.1.translation.x, ball.1.translation.y);
-
             let closest_anchor = anchor_query.iter().min_by(|(_, x), (_, y)| {
                 distance(&ball_point, &Point2::new(x.translation.x, x.translation.y))
                     .partial_cmp(&distance(
@@ -90,6 +94,7 @@ pub fn toggle_rope(
                     ))
                     .unwrap_or(Ordering::Equal)
             });
+
             if let Some(anchor) = closest_anchor {
                 // spawn rope between ball and anchor
                 spawn_rope(
@@ -107,4 +112,36 @@ pub fn toggle_rope(
             commands.entity(entity).despawn();
         }
     }
+}
+
+fn zip_to_point(
+    anchor_query: Query<(Entity, &Transform), With<AnchorPoint>>,
+    mut ball_query: Query<(Entity, &mut Transform), With<Ball>>,
+    keys: Res<Input<KeyCode>>,
+) {
+    if !keys.just_pressed(KeyCode::Space) {
+        return;
+    }
+
+    // get ball
+    let mut ball = ball_query.single_mut().unwrap();
+
+    // get closest anchor
+    let ball_point = Point2::new(ball.1.translation.x, ball.1.translation.y);
+    let closest_anchor = anchor_query
+        .iter()
+        .min_by(|(_, x), (_, y)| {
+            distance(&ball_point, &Point2::new(x.translation.x, x.translation.y))
+                .partial_cmp(&distance(
+                    &ball_point,
+                    &Point2::new(y.translation.x, y.translation.y),
+                ))
+                .unwrap_or(Ordering::Equal)
+        })
+        .unwrap();
+
+    let closest_anchor_point = Point2::new(
+        closest_anchor.1.translation.x,
+        closest_anchor.1.translation.y,
+    );
 }
